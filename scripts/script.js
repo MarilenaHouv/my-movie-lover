a0ec3f53f54c19396c6a768e27f46a90bce59cb6
Eleni Houvarda <houvarem@dukes.jmu.edu>
2024-04-25 11:56:40 -0400

    api implementation
    fetched movie info website, replaced image with a different animation

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,28 @@
 // let gifapi = "7FcrCZUTeZ4HSqxtoMcCVZlHiUnw2iz4";
 async function submit(){
-    console.log("hey");
-    alert("WORK");
+    document.getElementById("movielover").src = "images/ikdkman.gif";
     let x = document.getElementById("userinput").value;
     if (x == ""){
         alert("Invalid, type a movie name");
         return false;
     }
-    
+    //get gif
     let gifresp = await fetch(`http://api.giphy.com/v1/gifs/search?q=${x}&api_key=7FcrCZUTeZ4HSqxtoMcCVZlHiUnw2iz4&limit=5&rating=pg`);
     let respjson = await gifresp.json();
-    // let datar = JSON.stringify(respjson);
-    //  console.log(datar.data[0].images.original.url);
-    document.getElementById("gifimg").src = respjson.data[0].images.original.url;
-
+    document.getElementById("gifimg").src = respjson.data[0].images.original.url;   
+    
+    //get movie rating
+    const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer 16e8ab249fe9f83e43bde992793f46ed'
+        }
+      };
+      //https://api.themoviedb.org/3/search/movie?api_key=16e8ab249fe9f83e43bde992793f46ed&query=${shrek}&language=en-US
+      let movieresp = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=16e8ab249fe9f83e43bde992793f46ed&query=${x}&language=en-US`, options);
+      let mrespjson = await movieresp.json();
+      console.log(mrespjson);
+    
 }
 
